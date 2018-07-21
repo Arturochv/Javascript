fad0d86d9921bfbac4c168e888b2477ddc401652
Oscar López <lopezoscar@bibliotecainfinita.com>
2018-07-21 12:59:55 -0300

    correcciones tarea

--- clase_1/TareaClase1.js
+++ clase_1/TareaClase1.js
@@ -1,4 +1,4 @@
-/**1. Colocar un monto expresado en soles y luego imprimir el monto equivalente
+/** 1. Colocar un monto expresado en soles y luego imprimir el monto equivalente
 en dólares (con 2 dígitos decimales).
     Ej:(100 soles son 5.55 dolares)
 2. Elaborar un programa en el que se ingrese una cantidad expresada en segundos y
@@ -11,9 +11,11 @@ luego la exprese en días, horas, minutos y segundos.
 /**
 ejercicio 1 */
 var soles = 50
-var dolares = soles / 3.25
+var dolares = soles / 3.20
 
-console.log('50 soles en dolares son:' + ' ' + dolares )
+console.log('50 soles en dolares son:' + ' ' + dolares)
+console.log('50 soles en dolares son (truncados):' + ' ' + dolares.toFixed(2))
+console.log('50 soles en dolares son (redondeados):' + ' ' + Math.round(dolares * 100) / 100)
 
 /** EFERCICIO 2 */
 
@@ -22,72 +24,111 @@ var dias = 24
 var horas = 60
 var minutos = 60
 
-console.log(segundos+' '+'segundos'+ ' '+'en minutos:' +' ' + segundos / minutos)
-console.log(segundos+' '+'segundos'+' '+'en horas:' +' '+ segundos / minutos / horas)
-console.log(segundos+' '+'segundos'+' '+'en dias:' + ' '+ segundos / minutos / horas / dias)
+console.log(segundos + ' ' + 'segundos' + ' ' + 'en minutos:' + ' ' + segundos / minutos)
+console.log(segundos + ' ' + 'segundos' + ' ' + 'en horas:' + ' ' + segundos / minutos / horas)
+console.log(segundos + ' ' + 'segundos' + ' ' + 'en dias:' + ' ' + segundos / minutos / horas / dias)
 
 /** EJERCICIO 3 */
 
-var metros = 1210
+var metros = 1
 var centimetros = 100
 var milimetros = 1000
 var kilometros = 1000
 
-console.log(metros + ' '+ 'metros' + ' ' + 'en centimetros:' + ' ' + metros * centimetros +'cm')
-console.log(metros + ' '+ 'metros' + ' ' + 'en milimetros:' + ' ' + metros * milimetros +'mm')
-console.log(metros + ' '+ 'metros' + ' ' + 'en kilometros:' + ' ' + metros / kilometros +'km')
+console.log(metros + ' ' + 'metros' + ' ' + 'en centimetros:' + ' ' + metros * centimetros + 'cm')
+console.log(metros + ' ' + 'metros' + ' ' + 'en milimetros:' + ' ' + metros * milimetros + 'mm')
+console.log(metros + ' ' + 'metros' + ' ' + 'en kilometros:' + ' ' + metros / kilometros + 'km')
 
 /** EJERCICIO 4 */
+var texto = 'Vicente Arturo Chavez Villa'
+var partes = texto.split(' ')
+var sigla = ''
+for (var i = 0; i < partes.length; i++) {
+  sigla += partes[i][0]
+}
+console.log(sigla)
 
-var s = "Vicente Arturo Chavez Villa",
-    a = s.split(' ')
-    l = a.length,
+var s = 'Vicente Arturo Chavez Villa',
+  a = s.split(' ')
+l = a.length,
     i = 0,
-    n = "";
+    n = ''
 
-for (; i < l; ++i)
-{
-    n += a[i].charAt(0);
+for (; i < l; ++i) {
+  n += a[i].charAt(0)
 }
 
-console.log(n);
+console.log(n)
 
 /** EJERCICIO 5 */
 
-function convertPhoneLetters(input) {
-var inputlength = input.length;
-input = input.toLowerCase();
-var phonenumber = "";
-for (i = 0; i < inputlength; i++) {
-var character = input.charAt(i);
+function convertPhoneLetters (input) {
+  input = input.toLowerCase()
+  var phonenumber = ''
+  for (var i = 0; i < input.length; i++) {
+    var character = input[i]
+    switch (character) {
+      case 'a':
+      case 'b':
+      case 'c':
+        console.log('2')
+        phonenumber += '2'
+        break
+      case 'd':
+      case 'e':
+      case 'f':
+        console.log('3')
+        phonenumber += '3'
+        break
+      case 'g':
+      case 'h':
+      case 'i':
+        console.log('4')
+        phonenumber += '4'
+        break
+      case 'j':
+      case 'k':
+      case 'l':
+        console.log('5')
+        phonenumber += '5'
+        break
+      case 'm':
+      case 'n':
+      case 'o':
+        console.log('6')
+        phonenumber += '6'
+        break
+      case 'p':
+      case 'q':
+      case 'r':
+      case 's':
+        console.log('7')
+        phonenumber += '7'
+        break
+      case 't':
+      case 'u':
+      case 'v':
+        console.log('8')
+        phonenumber += '8'
+        break
+      case 'w':
+      case 'x':
+      case 'y':
+      case 'z':
+        console.log('9')
+        phonenumber += '9'
+        break
+      default:
+        console.log(character)
+        phonenumber += character
+        break
+    }
+    console.log(input)
+    console.log(phonenumber)
+  }
 
-
-switch(character) {
-
-case '0': phonenumber+="0";break;
-case '1': phonenumber+="1";break;
-case '2': phonenumber+="2";break;
-case '3': phonenumber+="3";break;
-case '4': phonenumber+="4";break;
-case '5': phonenumber+="5";break;
-case '6': phonenumber+="6";break;
-case '7': phonenumber+="7";break;
-case '8': phonenumber+="8";break;
-case '9': phonenumber+="9";break;
-
-case 'a': case 'b': case 'c': phonenumber+="2";break;
-case 'd': case 'e': case 'f': phonenumber+="3";break;
-case 'g': case 'h': case 'i': phonenumber+="4";break;
-case 'j': case 'k': case 'l': phonenumber+="5";break;
-case 'm': case 'n': case 'o': phonenumber+="6";break;''
-case 'p': case 'q': case 'r': case 's': phonenumber+="7";break;
-case 't': case 'u': case 'v': phonenumber+="8";break;
-case 'w': case 'x': case 'y': case 'z': phonenumber+="9";break;
-
-default: phonenumber+=character;
-
-   }
+  return phonenumber
 }
 
-return phonenumber;
-}
+convertPhoneLetters('0800-SUSANA')
+convertPhoneLetters('0800-SOS')
